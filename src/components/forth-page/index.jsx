c1a236d40ab79165c504f24069446f89a46d59f6
avetisyanmarine <marineavet2006@gmail.com>
2025-09-06 23:23:04 +0400

    change adding locaation

--- src/components/forth-page/index.jsx
+++ src/components/forth-page/index.jsx
@@ -8,10 +8,11 @@ export const ForthPage = () => {
                 <div>
                     <h2 className="text-[48px] font-[family-home]">Tsaghkunq Pool</h2>
                     <h2 className="text-[32px] font-[Vrdznagir] text-[var(--dark-color)]">Հասցե՝ էջմիածին, գյուղ Ծաղկունք</h2>
-                    <img className="w-full rounded-[30px] h-[250px]" loading="lazy" src={ImageSrc}  data-aos="fade-right" />
+                    <img className="w-full rounded-[30px] h-[250px]" loading="lazy" src={ImageSrc} data-aos="fade-right" />
                     <h3 className="text-[28px] font-[Vrdznagir] text-[var(--dark-color)]">Ինչպե՞ս հասնել</h3>
-                    <iframe  data-aos="fade-left" className="w-full rounded-[30px] h-[250px]"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24243.352895801523!2d44.83546099714829!3d40.57650340004458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40404881a8ebc3d9%3A0xa7ba3823f898c5ce!2z1L7VodWy1a_VuNaC1bbWhA!5e0!3m2!1shy!2sam!4v1755379976946!5m2!1shy!2sam"
+                    <iframe
+                        className="w-full rounded-[30px] h-[250px]"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3039.0434473985276!2d44.273364!3d40.173356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4041fc1f29f05d29%3A0x76835012409!2sԾաղկունք%20Pool!5e0!3m2!1shy!2sam!4v1234567890"
                         width="600"
                         height="450"
                         style={{ border: 0 }}
@@ -21,10 +22,10 @@ export const ForthPage = () => {
                     />
                 </div>
             </Container>
-                    <img src="/bant.png" alt="bant" className="w-full" />
-                <ForthPagePartContextLast>
-                    <h2 className="text-[32px] font-[Vrdznagir] text-[var(--dark-color)]">Եկեք միասին կիսենք <br />ուրախությունն ու ժպիտները</h2>
-                </ForthPagePartContextLast>
+            <img src="/bant.png" alt="bant" className="w-full" />
+            <ForthPagePartContextLast>
+                <h2 className="text-[32px] font-[Vrdznagir] text-[var(--dark-color)]">Եկեք միասին կիսենք <br />ուրախությունն ու ժպիտները</h2>
+            </ForthPagePartContextLast>
         </ForthPagePart>
     )
 }
